aa216588c138f70cac92c39f6164217b04ce4ea6
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2022-09-25 02:05:34 -0400

    todo

---
+++ examples/lec-10/todo/script.js
@@ -0,0 +1,51 @@
+const $ = (selector) => document.querySelector(selector);
+
+const todoList = $("#todos");
+const completedTasks = $("#completed");
+const todoForm = $("#todo-form");
+const input = $("#input");
+const todos = [];
+
+function todoChild(text, status) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.className = "todo-body";
+  span.textContent = text;
+
+  const button = document.createElement("button");
+
+  li.appendChild(span);
+  li.appendChild(button);
+  if (status) {
+    span.style.textDecoration = "line-through";
+    button.textContent = "🗑️";
+    button.addEventListener("click", () => {
+      removeTodo(li);
+    });
+  } else {
+    button.textContent = "✅";
+
+    button.addEventListener("click", () => {
+      completeTodo(li, text);
+    });
+  }
+  return li;
+}
+function createTodo(name) {
+  todos.push({name, status: false});
+  todoList.appendChild(todoChild(name));
+}
+function completeTodo(li, text) {
+  removeTodo(li);
+  completedTasks.appendChild(todoChild(text, true));
+}
+
+function removeTodo(li) {
+  li.remove();
+}
+
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  createTodo(input.value);
+  input.value = "";
+});
