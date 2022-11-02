8fec6bd5203ea4f73d0188ee04147fce4bca4ff2
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2022-11-02 02:01:48 -0400

    ok

--- demos/express-react-basic-form/backend/routes/users.js
+++ demos/express-react-basic-form/backend/routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+//
+
+const responses = {}; // ??
+router.post("/", (req, res) => {
+  console.log("do something??");
 });
 
 module.exports = router;
--- demos/express-react-basic-form/frontend/src/App.jsx
+++ demos/express-react-basic-form/frontend/src/App.jsx
@@ -13,11 +13,11 @@ function App() {
     // maybe a state here?
     // send a POST request
     // with JSON data
-    // name and college as 
+    // name and college as
     // {"name":name, "college":college}
     // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
-    const resp = await fetch("http://localhost:3000/user",{
-
+    const resp = await fetch("http://localhost:3000/user", {
+      // ??
     });
     const json = await resp.json();
     console.log(json);
