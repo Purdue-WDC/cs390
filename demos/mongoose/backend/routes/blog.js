995cb09a1204302373190e37797495e214add6b2
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2022-11-07 03:41:34 -0500

    comments

--- demos/mongoose/backend/routes/blog.js
+++ demos/mongoose/backend/routes/blog.js
@@ -6,14 +6,21 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get("/", async (req, res, next) => {
+  // find blogs based on no condition==> get all blogs
   const blogs = await BlogModel.find({});
+  // convert each blog to an object and send an array to client
   return res.send(blogs.map((blog) => blog.toObject()));
 });
 
 router.post("/create-post", async (req, res) => {
+  // body should be JSON
   const body = req.body;
+  // create blog model with the request body
   const blog = new BlogModel({content: body.content, title: body.title});
+  // remember to await .save();
+  // save to mongodb
   await blog.save();
+  // get an object representation and send it back to the client
   return res.send(blog.toObject());
 });
 
